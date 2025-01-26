4cad8c48f8f6a75b17715f854b71dcb2fd5a2176
rmistr00UAFS <157536646+rmistr00UAFS@users.noreply.github.com>
2025-01-26 00:02:45 -0600

    env fix

--- src/components/GetNotes.jsx
+++ src/components/GetNotes.jsx
@@ -9,10 +9,17 @@ export const GetNotes = ({ course }) => {
     useEffect(() => {
 
         fetchNotes(course).then(data => {
+
+            console.log(data)
+
+
+            data.sort((a, b) => {
+                // If ratings are the same, sort by newest date
+                return new Date(b.created_at) - new Date(a.created_at);
+            });
+
             setNotes(data)
         })
-
-        console.log(notes)
 
     }, [])
 
@@ -22,6 +29,14 @@ export const GetNotes = ({ course }) => {
         setIsFlipped(!isFlipped);
     };
 
+    let next=()=>{
+
+    }
+
+    let skip=()=>{
+        
+    }
+
 
     return <div id="notes">
 
@@ -29,24 +44,48 @@ export const GetNotes = ({ course }) => {
         {notes.length !== 0 &&
 
 
-            <div
-                className={`note-card ${isFlipped ? 'flipped' : ''}`}
-                onClick={handleFlip}
-            >
-                {/* Front Side */}
-                <div className="note-card-front">
-                    <h2>Problem</h2>
-                    <img src={notes[0]?.frontImgUrl} alt="Front Preview" />
+            <div className='note-card-container'>
+                <div id="card-actions">
+
+                <button id="card-skip-button"
+                        onClick={skip()}
+                    >skip</button>
+
+                    <button id="card-flip-button"
+                        onClick={handleFlip}
+                    >{isFlipped ? 'problem' : 'solution'}</button>
+
+                    <button id="card-next-button"
+                        onClick={next()}
+                    >next</button>
                 </div>
 
-                {/* Back Side */}
-                <div className="note-card-back">
-                    <h2>Solution</h2>
-                    <img src={notes[0]?.backImgUrl} alt="Back Preview" />
+                <div
+                    className={`note-card ${isFlipped ? 'flipped' : ''}`}
+                >
+
+                    {/* Front Side */}
+                    <div className="note-card-front">
+                        <h2>Problem</h2>
+                        <img src={notes[0]?.frontImgUrl} alt="Front Preview" />
+                    </div>
+
+                    {/* Back Side */}
+                    <div className="note-card-back">
+                        <h2>Solution</h2>
+                        <img src={notes[0]?.backImgUrl} alt="Back Preview" />
+                    </div>
                 </div>
             </div>
 
 
+
+
+        }
+
+        {
+
+notes.length == 0 &&<div id="notes">no cards</div>
 
         }
 
--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,22 @@ import { getFirestore,collection,getDocs,doc,getDoc,addDoc,deleteDoc } from "htt
 import { getStorage, ref, uploadBytes, getDownloadURL,deleteObject } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-storage.js";
 import { v4 as uuidv4 } from 'uuid'; // For generating unique filenames
 
-import {firebaseConfig} from "./api.js"
+
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
+
+
+
+
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage();
