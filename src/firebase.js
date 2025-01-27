ba4126effe795d03f79c222f66ff374cde441b40
rmistr00UAFS <157536646+rmistr00UAFS@users.noreply.github.com>
2025-01-26 23:35:44 -0600

    prototype

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getFirestore,collection,getDocs,doc,getDoc,addDoc,deleteDoc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
-import { getStorage, ref, uploadBytes, getDownloadURL,deleteObject } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-storage.js";
+import { getFirestore, collection, getDocs, doc, getDoc, addDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { getStorage, ref, uploadBytes, getDownloadURL, deleteObject } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-storage.js";
 import { v4 as uuidv4 } from 'uuid'; // For generating unique filenames
 
 
@@ -24,27 +24,27 @@ const db = getFirestore(app);
 const storage = getStorage();
 
 
-export async function fetchNotes(course) {
-    const notesCollection = collection(db, `courses/${course}/notes`);
-  
-    try {
-      const querySnapshot = await getDocs(notesCollection);
-  
-      if (querySnapshot.empty) {
-        console.log("No notes found!");
-        return [];
-      }
-  
-      const notes = querySnapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-        return notes;
-    } catch (error) {
-      console.error("Error fetching notes:", error);
+export async function firebaseGet(course) {
+  const notesCollection = collection(db, `courses/${course}/notes`);
+
+  try {
+    const querySnapshot = await getDocs(notesCollection);
+
+    if (querySnapshot.empty) {
+      console.log("No notes found!");
       return [];
     }
+
+    const notes = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    return notes;
+  } catch (error) {
+    console.error("Error fetching notes:", error);
+    return [];
   }
+}
 
 
 
@@ -54,7 +54,7 @@ export async function addNoteToFirestore(note) {
 
   try {
     const docRef = await addDoc(notesCollection, {
-    ...note,
+      ...note,
       created_at: new Date(),
     });
 
@@ -67,51 +67,55 @@ export async function addNoteToFirestore(note) {
 }
 
 
-export async function deleteNote(course,id) {
-    try {
-        const docRef = doc(db, `courses/${course}/notes/${id}`);
-        const docSnap = await getDoc(docRef);
-
-        await deleteDoc(docRef);
 
 
-          const { frontName, backName } = docSnap.data();
+export async function uploadImage(file, course) {
+  const filename = `courses/${course}/${uuidv4()}_${file.name}`; // Unique filename with UUID
 
- // Delete the front image
- const frontImageRef = ref(storage, frontName);
- await deleteObject(frontImageRef);
- console.log("Front image deleted successfully:", frontName);
+  try {
+    // Create a reference to the storage location
+    const storageRef = ref(storage, filename);
 
- // Delete the back image
- const backImageRef = ref(storage, backName);
- await deleteObject(backImageRef);
- console.log("Back image deleted successfully:", backName);
+    // Upload the file
+    console.log('uploading')
+    const snapshot = await uploadBytes(storageRef, file);
+    console.log("Uploaded image:", snapshot);
 
+    // Get the download URL
+    const url = await getDownloadURL(snapshot.ref);
 
-              return true
-    } catch (error) {
-        console.log(error)
-    }
+    return { filename, url };
+  } catch (error) {
+    console.error("Error uploading image:", error);
+    throw error;
   }
+}
 
 
-  export async function uploadImage(file, course) {
-    const filename = `courses/${course}/${uuidv4()}_${file.name}`; // Unique filename with UUID
 
-    try {
-      // Create a reference to the storage location
-      const storageRef = ref(storage, filename);
-  
-      // Upload the file
-      const snapshot = await uploadBytes(storageRef, file);
-      console.log("Uploaded image:", snapshot);
-  
-      // Get the download URL
-      const url = await getDownloadURL(snapshot.ref);
-  
-      return {filename,url}; 
-    } catch (error) {
-      console.error("Error uploading image:", error);
-      throw error;
-    }
+export async function firebaseDelete(course, id) {
+  try {
+    const docRef = doc(db, `courses/${course}/notes/${id}`);
+    const docSnap = await getDoc(docRef);
+
+    const { frontImgFileName
+      , backImgFileName } = docSnap.data();
+
+    await deleteDoc(docRef);
+
+    // Delete the front image
+    const frontImageRef = ref(storage, frontImgFileName);
+    await deleteObject(frontImageRef);
+    console.log("Front image deleted successfully:", frontImgFileName);
+
+    // Delete the back image
+    const backImageRef = ref(storage, backImgFileName);
+    await deleteObject(backImageRef);
+    console.log("Back image deleted successfully:", backImgFileName);
+
+
+    return true
+  } catch (error) {
+    console.log(error)
   }
+}
